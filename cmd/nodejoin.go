ab4975a106a76ee11a60f3447ee3b2d30ff16ec9
Daniel Lipovetsky <daniel@platform9.com>
2018-07-14 12:22:49 -0700

    Add cfg flag to join verb, read join configuration from file

--- cmd/nodejoin.go
+++ cmd/nodejoin.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"log"
+
 	"github.com/platform9/nodeadm/apis"
 	"github.com/platform9/nodeadm/constants"
 	"github.com/platform9/nodeadm/utils"
@@ -12,7 +14,15 @@ var nodeCmdJoin = &cobra.Command{
 	Use:   "join",
 	Short: "Initalize the worker node with given configuration",
 	Run: func(cmd *cobra.Command, args []string) {
-		var config *apis.JoinConfiguration
+		var err error
+		config := &apis.JoinConfiguration{}
+		configPath := cmd.Flag("cfg").Value.String()
+		if len(configPath) != 0 {
+			config, err = utils.JoinConfigurationFromFile(configPath)
+			if err != nil {
+				log.Fatalf("Failed to read configuration from file %q: %v", configPath, err)
+			}
+		}
 		apis.SetJoinDefaults(config)
 		utils.InstallWorkerComponents(config)
 		kubeadmJoin(cmd.Flag("token").Value.String(),
@@ -27,6 +37,7 @@ func kubeadmJoin(token, master, cahash string) {
 
 func init() {
 	rootCmd.AddCommand(nodeCmdJoin)
+	nodeCmdJoin.Flags().String("cfg", "", "Location of configuration file")
 	nodeCmdJoin.Flags().String("token", "", "kubeadm token to be used for kubeadm join")
 	nodeCmdJoin.Flags().String("master", "", "masterIP:masterPort for the master to join")
 	nodeCmdJoin.Flags().String("cahash", "", "CA hash")
