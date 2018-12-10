6b94d12de6fbaa83ab8d844717938609da50d5ca
puneet <puneet@platform9.com>
2018-12-10 12:05:44 -0800

    Upgrade kubernetes to 1.10.11 for CVE-2018-1002105

--- constants/constants.go
+++ constants/constants.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	KubernetesVersion                     = "v1.10.4"
+	KubernetesVersion                     = "v1.10.11"
 	CNIVersion                            = "v0.6.0"
 	BaseInstallDir                        = "/opt/bin"
 	CNIBaseDir                            = "/opt/cni/bin"
