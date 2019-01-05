fe573a3ac70b3ef6281f054fd0a279fda28f9318
Nirmoy Das <ndas@suse.de>
2019-01-06 00:34:07 +0100

    initialize ControllerManagerExtraArgs if its nil
    ControllerManagerExtraArgs has omitempty flag so it can be nil
    ./nodeadm  init --cfg=/tmp/nodeadm.yaml
    map[]panic: assignment to entry in nil map
    
    goroutine 1 [running]:
    github.com/platform9/nodeadm/apis.setControllerManagerExtraArgs(0xc00005d500)
    	/home/nirmoy/go/src/github.com/platform9/nodeadm/apis/workaround_kubeadm_issue_724.go:13 +0x24f
    github.com/platform9/nodeadm/apis.SetMasterConfigurationNetworkingDefaultsWithNetworking(0xc00005d500)
    	/home/nirmoy/go/src/github.com/platform9/nodeadm/apis/defaults.go:62 +0xa2
    github.com/platform9/nodeadm/apis.SetInitDefaults(0xc00005d500)
    	/home/nirmoy/go/src/github.com/platform9/nodeadm/apis/defaults.go:15 +0x8d
    github.com/platform9/nodeadm/cmd.glob..func3(0x1733620, 0xc000422200, 0x0, 0x1)
    	/home/nirmoy/go/src/github.com/platform9/nodeadm/cmd/nodeinit.go:35 +0xb8
    github.com/platform9/nodeadm/vendor/github.com/spf13/cobra.(*Command).execute(0x1733620, 0xc0004221e0, 0x1, 0x1, 0x1733620, 0xc0004221e0)
    	/home/nirmoy/go/src/github.com/platform9/nodeadm/vendor/github.com/spf13/cobra/command.go:766 +0x2cc
    github.com/platform9/nodeadm/vendor/github.com/spf13/cobra.(*Command).ExecuteC(0x1733d40, 0xc00031df68, 0xca0cef, 0xc000275100)
    	/home/nirmoy/go/src/github.com/platform9/nodeadm/vendor/github.com/spf13/cobra/command.go:852 +0x2fd
    github.com/platform9/nodeadm/vendor/github.com/spf13/cobra.(*Command).Execute(0x1733d40, 0x407470, 0xc00009a058)
    	/home/nirmoy/go/src/github.com/platform9/nodeadm/vendor/github.com/spf13/cobra/command.go:800 +0x2b
    github.com/platform9/nodeadm/cmd.Execute()
    	/home/nirmoy/go/src/github.com/platform9/nodeadm/cmd/root.go:30 +0x2d
    main.main()
    	/home/nirmoy/go/src/github.com/platform9/nodeadm/main.go:6 +0x20

--- apis/workaround_kubeadm_issue_724.go
+++ apis/workaround_kubeadm_issue_724.go
@@ -6,6 +6,9 @@ import (
 
 // SetControllerManagerExtraArgs sets controller manager extra args for a given pod network subnet
 func setControllerManagerExtraArgs(config *InitConfiguration) {
+	if config.MasterConfiguration.ControllerManagerExtraArgs == nil {
+		config.MasterConfiguration.ControllerManagerExtraArgs = make(map[string]string)
+	}
 	if _, ok := config.MasterConfiguration.ControllerManagerExtraArgs[constants.ControllerManagerAllocateNodeCIDRsKey]; !ok {
 		config.MasterConfiguration.ControllerManagerExtraArgs[constants.ControllerManagerAllocateNodeCIDRsKey] = constants.ControllerManagerAllocateNodeCIDRs
 	}
