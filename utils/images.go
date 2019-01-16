62abb30613679c187365356e9fbfee6b9f0e972f
Daniel Lipovetsky <daniel@platform9.com>
2019-01-15 17:43:34 -0800

    Download versioned, arch-agnostic pause image
    kubeadm 1.11 expects the arch-agnostic image. See https://github.com/kubernetes/kubernetes/commit/0745b257dd0a0e77761d4793c219b2b161d0a222

--- utils/images.go
+++ utils/images.go
@@ -14,7 +14,7 @@ var DOCKER_IMAGES = []string{
 	fmt.Sprintf("k8s.gcr.io/kube-proxy-amd64:%s", constants.KubernetesVersion),
 	fmt.Sprintf("k8s.gcr.io/coredns:%s", constants.CoreDNSVersion),
 	"quay.io/coreos/flannel:v0.10.0-amd64",
-	"k8s.gcr.io/pause-amd64:3.1",
+	fmt.Sprintf("k8s.gcr.io/pause:%s", constants.PauseContainerVersion),
 	"metallb/speaker:master",
 	"metallb/controller:master",
 }
